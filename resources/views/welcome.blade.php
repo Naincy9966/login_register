46bc581c11b89db169dbf8d79c263bb88abdd06b
Laravel <2355876@swansea.ac.uk>
2023-11-04 23:52:24 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Include Bootstrap CSS via a CDN -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+    <div class="text-center">
+        <button class="btn btn-primary">
+            <a href="login" class="text-white text-decoration-none">Login here</a>
+        </button>
+
+        <button class="btn btn-primary">
+            <a href="register" class="text-white text-decoration-none">Register here</a>
+        </button>
+    </div>
+</div>
+
+<!-- Include jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Include Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
